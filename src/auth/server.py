936724df538ce252bac7bcad16be59352c051a51
Swayam Sharma <93614727+swayamsharma16@users.noreply.github.com>
2024-08-24 21:50:33 +0530

    fixed typo in return message of login function in auth file

--- src/auth/server.py
+++ src/auth/server.py
@@ -35,7 +35,7 @@ def login():
         else:
             return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
-        return "invalide credentials", 401
+        return "invalid credentials", 401
 
 
 @server.route("/validate", methods=["POST"])
